a24a072b526e8996ba6aefd65487ebfc61c4f332
moru <kevin.singer@student.tuwien.ac.at>
2015-09-07 23:59:27 +0200

    Refactored.

--- Client/app/wstest.js
+++ Client/app/wstest.js
@@ -8,25 +8,27 @@ export function webSocketStreams(url) {
     return (send$) => {
         let ws = new WebSocket(url);
 
-        send$.subscribe(x => ws.send(x));
+        // enable sending after the socket opens
+        ws.onopen = function() {
+            send$.subscribe(x => ws.send(x));
+        }
 
-        let receive$ = Rx.Observable
-            .create(observer => {
-                ws.onmessage = (msg, flags) => {
-                    console.log('received - msg: ', msg);
-                    console.log('received - flags: ', flags);
-                    observer.onNext(msg);
-                }
-                ws.onerror = (args) => {
-                    console.error('websocket error: ', args);
-                    observer.onError(args);
-                }
-                ws.onerror = (args) => {
-                    console.error('websocket closed: ', args);
-                    //TODO try reconnecting first
-                    observer.onClose(args);
-                }
-            });
-        return receive$;
+        // receiving messages and errors
+        return Rx.Observable.create(observer => {
+            ws.onmessage = (msg, flags) => {
+                console.log('received - msg: ', msg);
+                console.log('received - flags: ', flags);
+                observer.onNext(msg);
+            }
+            ws.onerror = (args) => {
+                console.error('websocket error: ', args);
+                observer.onError(args);
+            }
+            ws.onerror = (args) => {
+                console.error('websocket closed: ', args);
+                //TODO try reconnecting first
+                observer.onClose(args);
+            }
+        });
     }
 }
