65c9457ceb24c206695e5bc45de39e7f026ecb91
moru <kevin.singer@student.tuwien.ac.at>
2015-09-07 23:56:15 +0200

    Refactored.

--- Client/app/wstest.js
+++ Client/app/wstest.js
@@ -7,46 +7,26 @@ import {Rx} from '@cycle/core';
 export function webSocketStreams(url) {
     return (send$) => {
         let ws = new WebSocket(url);
-        return new Promise((resolve, reject) => {
-            ws.onopen = () => {
-                console.log('websocket open');
-                receive$ = _fromWebsocket(ws, send$)
 
-                //TODO for debugging, deletme
-                window.ws = ws;
-                window.receive$ = receive$;
+        send$.subscribe(x => ws.send(x));
 
-                resolve(receive$);
-            }
-            ws.onerror = (args) => {
-                console.error('websocket closed: ', args);
-                reject("Couldn't open web-socket to " + url);
-            }
-        })
+        let receive$ = Rx.Observable
+            .create(observer => {
+                ws.onmessage = (msg, flags) => {
+                    console.log('received - msg: ', msg);
+                    console.log('received - flags: ', flags);
+                    observer.onNext(msg);
+                }
+                ws.onerror = (args) => {
+                    console.error('websocket error: ', args);
+                    observer.onError(args);
+                }
+                ws.onerror = (args) => {
+                    console.error('websocket closed: ', args);
+                    //TODO try reconnecting first
+                    observer.onClose(args);
+                }
+            });
+        return receive$;
     }
-}
-/**
- * NOTE: can only be called once on each web-socket as
- * it attaches callback-function to predermined properties.
- */
-function _fromWebsocket(ws) {
-    let receive$ = Rx.Observable
-        .create(observer => {
-            ws.onmessage = (msg, flags) => {
-                console.log('received - msg: ', msg);
-                console.log('received - flags: ', flags);
-                observer.onNext(msg);
-            }
-            ws.onerror = (args) => {
-                console.error('websocket error: ', args);
-                observer.onError(args);
-            }
-            ws.onerror = (args) => {
-                console.error('websocket closed: ', args);
-                observer.onClose(args);
-            }
-        });
-    send$.subscribe(x => ws.send(x));
-
-    return receive$;
 }
