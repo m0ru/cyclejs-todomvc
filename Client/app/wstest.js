b28371706d96140b6b2059449f94d617678a19cf
moru <kevin.singer@student.tuwien.ac.at>
2015-09-07 23:50:50 +0200

    Made socketfun curryable.

--- Client/app/wstest.js
+++ Client/app/wstest.js
@@ -2,26 +2,28 @@ import WebSocket from 'ws';
 import {Rx} from '@cycle/core';
 
 
-var ws = new WebSocket('ws://localhost:8080');
+//var ws = new WebSocket('ws://localhost:8080');
 
-export function webSocketStreams(url, send$) {
-    let ws = new WebSocket(url);
-    return new Promise((resolve, reject) => {
-        ws.onopen = () => {
-            console.log('websocket open');
-            receive$ = _fromWebsocket(ws, send$)
+export function webSocketStreams(url) {
+    return (send$) => {
+        let ws = new WebSocket(url);
+        return new Promise((resolve, reject) => {
+            ws.onopen = () => {
+                console.log('websocket open');
+                receive$ = _fromWebsocket(ws, send$)
 
-            //TODO for debugging, deletme
-            window.ws = ws;
-            window.receive$ = receive$;
+                //TODO for debugging, deletme
+                window.ws = ws;
+                window.receive$ = receive$;
 
-            resolve(receive$);
-        }
-        ws.onerror = (args) => {
-            console.error('websocket closed: ', args);
-            reject("Couldn't open web-socket to " + url);
-        }
-    })
+                resolve(receive$);
+            }
+            ws.onerror = (args) => {
+                console.error('websocket closed: ', args);
+                reject("Couldn't open web-socket to " + url);
+            }
+        })
+    }
 }
 /**
  * NOTE: can only be called once on each web-socket as
