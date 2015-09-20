d5eba8671422acd4dc3c5c185c82e31cbc71a4b9
moru <kevin.singer@student.tuwien.ac.at>
2015-09-20 21:58:35 +0200

    Playing with json-serving and upgrade to ws.

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,8 +1,51 @@
 //require('./jspm_packages/system');
 //require('./jspm.config');
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8080});
+var http = require('http');
 
+// Create an HTTP server
+var httpServer = http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({ a: 1 }));
+});
+httpServer.on('upgrade', function(req, socket, head) {
+  socket.write('HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
+               'Upgrade: WebSocket\r\n' +
+               'Connection: Upgrade\r\n' +
+               '\r\n');
+
+  socket.pipe(socket); // echo back
+});
+httpServer.listen(3000);
+/*
+
+// now that server is running
+srv.listen(3000, '127.0.0.1', function() {
+
+  // make a request
+  var options = {
+    port: 3000,
+    hostname: '127.0.0.1',
+    headers: {
+      'Connection': 'Upgrade',
+      'Upgrade': 'websocket'
+    }
+  };
+
+  var req = http.request(options);
+  req.end();
+
+  req.on('upgrade', function(res, socket, upgradeHead) {
+    console.log('got upgraded!');
+    socket.end();
+    process.exit(0);
+  });
+});
+
+*/
+
+
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({port: 8080});
 let wsCount = 0;
 let sockets = new Map();
 wss.on('connection', function(thisWs) {
